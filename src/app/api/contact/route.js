ba8f2c9a2c16237b555648b54677216aeb2cb6c3
syedhassan123 <syed49420@gmail.com>
2024-07-22 19:04:30 +0500

    Updated Key

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,15 +1,15 @@
 const { NextResponse } = require("next/server");
-import { connection } from "@/utils/dbConnect";
+// import { connection } from "@/utils/dbConnect";
 import { Contact } from "@/utils/model/schema";
 import mongoose from "mongoose";
-
+const connection=process.env.DB_KEY
 // Function to get the data from database
-// export async function GET(req){
-//     await mongoose.connect(connection)
-//     const data=await Contact.find()
-//     console.log(data);
-//     return NextResponse.json({result:data})
-// }
+export async function GET(req){
+    await mongoose.connect(connection)
+    const data=await Contact.find()
+    console.log(data);
+    return NextResponse.json({result:data})
+}
 
 // export async function POST(req, res) {
 //   const payload = await req.json();
--- src/utils/dbConnect.js
+++ src/utils/dbConnect.js
@@ -1,9 +1,9 @@
-const username=process.env.DB_USERNAME
-const password=process.env.DB_PASSWORD
+// const username=process.env.DB_USERNAME
+// const password=process.env.DB_PASSWORD
 
-console.log("username" , username);
-console.log("password" , password);
+// console.log("username" , username);
+// console.log("password" , password);
 
 
-export const connection=`mongodb+srv://${username}:${password}@cluster0.7pvt8xf.mongodb.net/user-data?retryWrites=true&w=majority&appName=Cluster0`
+// export const connection=`mongodb+srv://${username}:${password}@cluster0.7pvt8xf.mongodb.net/user-data?retryWrites=true&w=majority&appName=Cluster0`
 
